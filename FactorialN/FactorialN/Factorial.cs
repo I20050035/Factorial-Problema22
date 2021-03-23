f323d58a555e5af907d6e8ef9ed83c57435eb351
I20050035 <70992098+I20050035@users.noreply.github.com>
2021-03-23 17:50:01 -0600

    Add files via upload

---
+++ FactorialN/FactorialN/Factorial.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactorialN
+{
+    class Factorial
+    { 
+
+        //variable y propiedad
+        int num;
+        public void Num_set(int valor)
+        {
+            num = valor;
+        }
+        //variable y propiedad
+        int fac;
+        public int fac_regresar()
+        {
+            return fac;
+        }
+        //metodo para calcular el factorial
+        public void calcular()
+        {
+            int i;
+            fac = num;
+
+            for (i= 1; i < num;i++)
+            {
+                fac = fac * i;
+            }
+        }
+    }
+}
---
+++ FactorialN/FactorialN/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FactorialN
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           Factorial clase = new  Factorial();
+            //ingresar el numero
+            clase.Num_set(int.Parse(textBox1.Text));
+            //llamar el metodo para calcular
+            clase.calcular();
+            //mostrar resultado
+            textBox2.Text = clase.fac_regresar() + "";
+            MessageBox.Show("El factor es : "+clase.fac_regresar()+"");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //para salir 
+            DialogResult salir;
+            salir = MessageBox.Show("Deseas salir de la aplicacion?", "Exit", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (salir == DialogResult.Yes)
+            {
+                Close();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //borrar contenido del textbox
+            textBox1.Clear();
+            textBox2.Clear();
+        }
+    }
+}
